8984b2182919da7fbf5556cf3771fefcd0491b9a
chistorrita <cristo11burgos@gmail.com>
2025-12-11 01:18:57 +0100

    update

--- loader.js
+++ loader.js
@@ -1,21 +1,13 @@
-// Abre juegos como BLOB cargándolos desde GitHub
 async function abrirJuegoBlob(ruta) {
     try {
-        // Cargar el archivo HTML del juego
+        if(ruta.endsWith('/')) ruta += 'index.html';
         let respuesta = await fetch(ruta);
         let texto = await respuesta.text();
-
-        // Convertir el HTML en un Blob
         let blob = new Blob([texto], { type: "text/html" });
-
-        // Crear URL tipo blob:
         let blobURL = URL.createObjectURL(blob);
-
-        // Abrir en nueva ventana
         let w = window.open(blobURL, "_blank");
-        if (!w) alert("La ventana fue bloqueada.");
-
-    } catch (e) {
+        if(!w) alert("La ventana fue bloqueada.");
+    } catch(e) {
         alert("Error cargando el juego: " + e);
     }
 }
